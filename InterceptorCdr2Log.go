337850d18d7094daca57c303593606aedf4c7eff
Fulldump <gerardooscarjt@gmail.com>
2016-10-26 15:18:17 +0200

    fix doc

--- InterceptorCdr2Log.go
+++ InterceptorCdr2Log.go
@@ -23,7 +23,7 @@ import (
 // Here is the sample output:
 //
 // 	2016/10/24 19:49:19 CDR {"id":"580a465bce507629a613107c","version":"1.0.0","consumer_id":"my-consumer-id","origin":"127.0.0.1","session_id":"","service":"invented-service","entry_date":"2016-10-21T18:46:19.820423299+02:00","entry_timestamp":1.4770683798204234e+09,"elapsed_seconds":1.621246337890625e-05,"request":{"method":"POST","uri":"/value-1/value-2/test-node?query_a=aaa\u0026query_b=bbb","handler":"/{param1}/{param2}/test-node","args":{"query_a":["aaa"],"query_b":["bbb"]},"length":2},"response":{"status_code":222,"length":5,"error":{"code":27,"description":"my-error-description"}},"read_access":["other-involved-consumer-id","my-consumer-id"],"custom":{"a":20,"b":55}}
-
+//
 func InterceptorCdr2Log() *golax.Interceptor {
 	return &golax.Interceptor{
 		Documentation: golax.Doc{
