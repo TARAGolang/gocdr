18bc657bbde8d75a54e901f5e2d77c4d2f43a431
Fulldump <gerardooscarjt@gmail.com>
2016-10-26 16:49:55 +0200

    add doc for Chan2Mongo

--- utils/chan2mongo.go
+++ utils/chan2mongo.go
@@ -8,6 +8,28 @@ import (
 	"github.com/smartdigits/gocdr/model"
 )
 
+// Chan2Mongo is a util to extract CDRs from a channel and store to a MongoDB
+// collection. This interceptor should wrap `InterceptorCdr`.
+//
+// Note that this interceptor do not extract the CDRs from the channel, that should
+// be done by other task. There is a util included (`Chan2Mongo`) to do that.
+//
+// Typical usage:
+// 	channel_cdrs := make(chan *model.CDR, 100)  // Buffered channel, 100 items
+//
+// 	collection_cdrs := mongo_db.C("cdrs") // assume `mongo_db` already exists
+//
+// 	Chan2Mongo(channel_cdrs, collection_cdrs) // do the job: channel -> mongo
+//
+// 	a := golax.NewApi()
+//
+// 	a.Root.
+// 	    Interceptor(InterceptorCdr2Channel(channel_cdrs)). // Pass created channel
+// 	    Interceptor(InterceptorCdr("invented-service")).
+// 	    Method("GET", func(c *golax.Context) {
+// 	        // Implement your API here
+// 	    })
+//
 func Chan2Mongo(s chan *model.CDR, m *mgo.Collection) {
 
 	go func() {
